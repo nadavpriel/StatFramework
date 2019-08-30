3e96c2e5499ddc23f74653cc182486714f68388c
nadavpriel <nadavpriel@gmail,com>
2019-08-30 16:43:47 -0700

    sine template fit

--- likelihood_calculator/likelihood_analyser.py
+++ likelihood_calculator/likelihood_analyser.py
@@ -7,10 +7,10 @@ import pprint
 
 class LikelihoodAnalyser:
     def __init__(self):
-        print('Constructor')
         self.data_x = 0  # x-data to fit
         self.data_y = 0  # y-data to fit
         self.fsamp = 0  # sampling rate
+        self.noise_sigma = 1  # gaussian white noise std
 
     def least_squares_sine(self, A, f, phi):
         """
@@ -22,9 +22,9 @@ class LikelihoodAnalyser:
         """
         func_t = A * np.sin(2 * np.pi * f * self.data_x + phi)  # function to minimize
         res = sum(np.power(np.abs(self.data_y - func_t), 2))
-        return res / 100
+        return res / self.noise_sigma ** 2
 
-    def find_mle_sin(self, x, drive_freq=0, fsamp=5000, bandwidth=50, plot=False):
+    def find_mle_sin(self, x, drive_freq=0, fsamp=5000, bandwidth=50, noise_rms=0, plot=False, **kwargs):
         """
         The function is fitting the data with a sine template using iminuit.
         The fitting is done after applying a bandpass filter.
@@ -35,27 +35,23 @@ class LikelihoodAnalyser:
         :param drive_freq: drive frequency of the response
         :return: estimated values, chi_square
         """
-        print('Starting to fit a sine template')
-        print('data overall time: ', len(x) / fsamp, ' sec.')
+        print('Data overall time: ', len(x) / fsamp, ' sec.')
         self.fsamp = fsamp
+        if noise_rms != 0:
+            self.noise_sigma = noise_rms
 
         # apply bandpass filter to data and store data in the correct place for the minimization
         self.data_x = np.arange(0, len(x)) / fsamp
         if drive_freq != 0:
-            print('Bandpass filter on. Bandwidth: ', bandwidth)
+            print('Bandpass filter on. Bandwidth: ', bandwidth, 'Hz')
             b, a = signal.butter(3, [2. * (drive_freq - bandwidth / 2.) / self.fsamp,
                                      2. * (drive_freq + bandwidth / 2.) / self.fsamp], btype='bandpass')
             self.data_y = signal.filtfilt(b, a, x)
         else:
             self.data_y = x
 
-        # fit arguments
-        fit_kwargs = {'A': 5, 'f': 100, 'phi': 0, 'error_A': 2, 'error_f': 10, 'error_phi': 0.5, 'errordef': 0.5,
-                      'limit_phi': [0, 2 * np.pi],
-                      'print_level': 0, 'fix_f': True, 'fix_phi': False}
-
         # we create an instance of Minuit and pass the function to minimize
-        m = Minuit(self.least_squares_sine, **fit_kwargs)
+        m = Minuit(self.least_squares_sine, **kwargs)
         m.migrad(ncall=50000)
         print(m.get_param_states())
 
@@ -64,13 +60,11 @@ class LikelihoodAnalyser:
             ax[0].scatter(self.data_x, self.data_y)
             fft = np.abs(np.fft.rfft(x)) ** 2
             freq = np.fft.rfftfreq(len(x), d=1. / fsamp)
-            ax[1].semilogy(freq, fft)
-            ax[1].set(xlim=(1, 500))
+            ax[0].set(title='raw data')
+            plt.subplot(122)
+            m.draw_profile('A', subtract_min=True)
             plt.show()
 
         print('reduced chi2: ', m.fval / (len(self.data_y) - 2))
-
-        m.draw_profile('A', subtract_min=True)
-        plt.show()
 
         return m
