09a8822fa2ea24b4dd268f785e5bf0cc5a7a5d2c
nadavpriel <nadavpriel@gmail,com>
2019-09-09 18:30:59 -0700

    added cython fit version 0 (before renaming and cleaning)

--- Main_simple_sine_fit.py
+++ Main_simple_sine_fit.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 from likelihood_calculator import likelihood_analyser
+import time
+
+import LeastSquares
 
 print('************************')
 print('Starting simple sine fit')
@@ -9,12 +12,12 @@ print('************************')
 lc_i = likelihood_analyser.LikelihoodAnalyser()
 
 # generating fake data
-samples = 5000
-noise_rms = 10
-time = np.arange(0, samples) / 5000
+samples = 500000
+noise_rms = 1
+time_x = np.arange(0, samples) / 5000
 freq = 100
 amp = 1
-sig_x = amp * np.sin(2 * np.pi * freq * time) + noise_rms * np.random.randn(samples)
+sig_x = amp * np.sin(2 * np.pi * freq * time_x) + noise_rms * np.random.randn(samples)
 
 x = np.random.randn(samples)
 
@@ -22,6 +25,6 @@ x = np.random.randn(samples)
 fit_kwargs = {'A': 5, 'f': 100, 'phi': 0, 'error_A': 2, 'error_f': 10, 'error_phi': 0.5, 'errordef': 0.5,
               'limit_phi': [0, 2 * np.pi],
               'print_level': 0, 'fix_f': True, 'fix_phi': False}
+m = lc_i.find_mle_sin(sig_x, drive_freq=freq, noise_rms=noise_rms, bandwidth=100, plot=False, **fit_kwargs)
 
-m = lc_i.find_mle_sin(sig_x, drive_freq=freq, noise_rms=noise_rms, bandwidth=100, plot=True, **fit_kwargs)
-
+m = lc_i.find_mle_sin2(sig_x, drive_freq=freq, noise_rms=noise_rms, bandwidth=100, plot=False, **fit_kwargs)
--- likelihood_calculator/likelihood_analyser.py
+++ likelihood_calculator/likelihood_analyser.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from iminuit import Minuit
-import pprint
+import LeastSquares
+import time
 
 
 class LikelihoodAnalyser:
@@ -53,7 +54,61 @@ class LikelihoodAnalyser:
 
         # we create an instance of Minuit and pass the function to minimize
         m = Minuit(self.least_squares_sine, **kwargs)
+
+        start = time.time()
         m.migrad(ncall=50000)
+        end = time.time()
+        print('time: ', end - start)
+        print(m.get_param_states())
+
+        if plot:
+            _, ax = plt.subplots(1, 2, figsize=(9.5, 4))
+            ax[0].scatter(self.data_x, self.data_y)
+            fft = np.abs(np.fft.rfft(x)) ** 2
+            freq = np.fft.rfftfreq(len(x), d=1. / fsamp)
+            ax[0].set(title='raw data')
+            plt.subplot(122)
+            m.draw_profile('A', subtract_min=True)
+            plt.show()
+
+        print('reduced chi2: ', m.fval / (len(self.data_y) - 2))
+
+        return m
+
+    def find_mle_sin2(self, x, drive_freq=0, fsamp=5000, bandwidth=50, noise_rms=0, plot=False, **kwargs):
+        """
+        The function is fitting the data with a sine template using iminuit.
+        The fitting is done after applying a bandpass filter.
+        :param noise_rms: std of the white gaussian noise
+        :param plot: plot the data and its fft
+        :param bandwidth: bandwidth for butter filter [Hz]
+        :param fsamp: sampling rate [1/sec]
+        :param x: 1 dim. position data (time domain)
+        :param drive_freq: drive frequency of the response
+        :return: estimated values, chi_square
+        """
+        print('Data overall time: ', len(x) / fsamp, ' sec.')
+        self.fsamp = fsamp
+        if noise_rms != 0:
+            self.noise_sigma = noise_rms
+
+        # apply bandpass filter to data and store data in the correct place for the minimization
+        self.data_x = np.arange(0, len(x)) / fsamp
+        if drive_freq != 0:
+            print('Bandpass filter ON. Bandwidth: ', bandwidth, 'Hz')
+            b, a = signal.butter(3, [2. * (drive_freq - bandwidth / 2.) / self.fsamp,
+                                     2. * (drive_freq + bandwidth / 2.) / self.fsamp], btype='bandpass')
+            self.data_y = signal.filtfilt(b, a, x)
+        else:
+            self.data_y = x
+        ls = LeastSquares.LeastSquares(self.data_x, self.data_y)
+
+        # we create an instance of Minuit and pass the function to minimize
+        m = Minuit(ls.compute, **kwargs)
+        start = time.time()
+        m.migrad(ncall=50000)
+        end = time.time()
+        print('time: ', end - start)
         print(m.get_param_states())
 
         if plot:
