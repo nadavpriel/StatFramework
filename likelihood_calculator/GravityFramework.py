261a400bf6b345a928b82e972c7eeecd181bf567
nadavpriel <nadavpriel@gmail,com>
2019-12-17 20:15:18 -0800

    with build_harmonics_array - one harmonic

--- likelihood_calculator/GravityFramework.py
+++ likelihood_calculator/GravityFramework.py
@@ -100,9 +100,9 @@ class GravityFramework:
         harmonic = 1
         fit_kwargs = {'A': 10, 'f': drive_freq, 'phi': 0, 'A2': 2, 'f2': drive_freq,
                       'delta_phi': 0,
-                      'error_A': 1, 'error_f': 1, 'error_phi': 0.1, 'errordef': 1,
+                      'error_A': 1, 'error_f': 1, 'error_phi': 0.5, 'errordef': 1,
                       'error_A2': 1, 'error_f2': 1, 'error_delta_phi': 0.1,
-                      'limit_phi': [0, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
+                      'limit_phi': [-2 * np.pi, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
                       'limit_A': [0, 1000], 'limit_A2': [0, 1000],
                       'print_level': 0, 'fix_f': True, 'fix_phi': False, 'fix_f2': True, 'fix_delta_phi': True,
                       'fix_A2': False}
@@ -132,9 +132,9 @@ class GravityFramework:
 
         fit_kwargs = {'A': 0, 'f': freq, 'phi': 0, 'A2': self.A2_mean, 'f2': freq,
                       'delta_phi': 0,
-                      'error_A': 1, 'error_f': 1, 'error_phi': 0.1, 'errordef': 1,
+                      'error_A': 1, 'error_f': 1, 'error_phi': 0.5, 'errordef': 1,
                       'error_A2': 1, 'error_f2': 1, 'error_delta_phi': 0.1,
-                      'limit_phi': [0, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
+                      'limit_phi': [-2 * np.pi, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
                       'limit_A': [0, 1000], 'limit_A2': [0, 1000],
                       'print_level': 0, 'fix_f': True, 'fix_phi': False, 'fix_f2': True, 'fix_delta_phi': True,
                       'fix_A2': True}
