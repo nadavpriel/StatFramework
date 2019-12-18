01a183bf27d76a937efe66720e5527e85e4b17fe
nadavpriel <nadavpriel@gmail,com>
2019-12-17 20:39:22 -0800

    with build_harmonics_array - one harmonic

--- likelihood_calculator/GravityFramework.py
+++ likelihood_calculator/GravityFramework.py
@@ -134,7 +134,7 @@ class GravityFramework:
                       'delta_phi': 0,
                       'error_A': 1, 'error_f': 1, 'error_phi': 0.5, 'errordef': 1,
                       'error_A2': 1, 'error_f2': 1, 'error_delta_phi': 0.1,
-                      'limit_phi': [-2 * np.pi, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
+                      'limit_phi': [-np.pi, np.pi], 'limit_delta_phi': [-0.1, 0.1],
                       'limit_A': [0, 1000], 'limit_A2': [0, 1000],
                       'print_level': 0, 'fix_f': True, 'fix_phi': False, 'fix_f2': True, 'fix_delta_phi': True,
                       'fix_A2': True}
