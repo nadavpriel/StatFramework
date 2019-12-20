a5fb4e503c2bbda401eb60f0867ee63b7496cd6a
nadavpriel <nadavpriel@gmail,com>
2019-12-19 21:27:34 -0800

    fit force template

--- likelihood_calculator/likelihood_analyser.py
+++ likelihood_calculator/likelihood_analyser.py
@@ -99,9 +99,9 @@ class LikelihoodAnalyser:
                                  2. * (center_freq + bandwidth / 2.) / self.fsamp], btype='bandpass')
         self.data_y = signal.filtfilt(b, a, x)[5000:-5000]
         self.template = signal.filtfilt(b, a, template)[5000:-5000]
-        _,ax = plt.subplots()
-        ax.scatter(range(self.data_y),self.data_y)
-        ax.scatter(range(self.data_y), self.template)
+        _, ax = plt.subplots()
+        ax.scatter(range(40000),self.data_y)
+        ax.scatter(range(40000), self.template)
 
         mimuit_minimizer = Minuit(self.least_squares_template, **kwargs)
         mimuit_minimizer.migrad(ncall=50000)
