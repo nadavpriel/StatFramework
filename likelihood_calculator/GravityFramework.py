04125eeca847e20782d337d17b8c75bf78eb464d
nadavpriel <nadavpriel@gmail,com>
2020-01-14 20:17:37 -0800

    bug fixed

--- likelihood_calculator/GravityFramework.py
+++ likelihood_calculator/GravityFramework.py
@@ -103,7 +103,7 @@ class GravityFramework:
                                         plot=False, suppress_print=True, **fit_kwargs)
 
         print('***************************************************')
-        print('X2-amplitude: ', '{:.2e}'.format(np.abs(m1_tmp.values[0])))
+        print('Z2-amplitude: ', '{:.2e}'.format(np.abs(m1_tmp.values[0])))
         print('reduced chi2: ', m1_tmp.fval / (len(xx2) - 2))
 
         return m1_tmp.values[0], m1_tmp.errors[0], m1_tmp
@@ -214,7 +214,7 @@ class GravityFramework:
                       'error_A': 1, 'error_f': 1, 'error_phi': 0.5, 'errordef': 1,
                       'error_A2': 1, 'error_f2': 1, 'error_delta_phi': 0.1,
                       'limit_phi': [-2 * np.pi, 2 * np.pi], 'limit_delta_phi': [-0.1, 0.1],
-                      'limit_A': [0, 1000], 'limit_A2': [0, 1000],
+                      'limit_A': [0, 100000], 'limit_A2': [0, 1000],
                       'print_level': 0, 'fix_f': True, 'fix_phi': False, 'fix_f2': True, 'fix_delta_phi': True,
                       'fix_A2': False}
 
