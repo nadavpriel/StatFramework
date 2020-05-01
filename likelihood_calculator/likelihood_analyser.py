667120b4391cb8b2bd285c5c170ad166dada24fe
nadavpriel <nadavpriel@gmail,com>
2020-04-30 23:22:04 -0700

    likelihood fit of multiple harmoincs added

--- likelihood_calculator/likelihood_analyser.py
+++ likelihood_calculator/likelihood_analyser.py
@@ -121,9 +121,10 @@ class LikelihoodAnalyser:
         """
 
         res = 0
+        A_all = sum(self.harmoincs_amp)
         for A_, phi_, f_, data_ in zip(self.harmoincs_amp, self.harmoincs_phases, self.harmoincs_freqs, self.data_y):
             func_t = A * A_ * np.sin(2 * np.pi * f_ * self.data_x + phi_ + phi)  # function to minimize
-            res += sum(np.power(np.abs(data_ - func_t), 2))
+            res += sum(np.power(np.abs(data_ - func_t), 2))*A_/A_all
         # print('A = ', A, 'phi = ', phi, 'res = ', res/1e6)
 
         return res/1e6
